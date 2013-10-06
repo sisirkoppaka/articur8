62d39a1384d78bfaf440af45433a92370b51d033
Sisir Koppaka <sisir.koppaka@gmail.com>
2013-10-05 23:58:00 -0400

    Eliminate the need for shell scripts for motherlode
    Turn on motherlode with 'fab start_motherlode', turn it off with 'fab stop_motherlode' anywhere in the articurate module tree.

---
+++ fabfile.py
@@ -0,0 +1,32 @@
+from fabric.api import *
+from fabric.colors import green, red
+
+import psutil
+import re
+
+#Start Motherlode
+def start_motherlode(debug='False'):
+	if debug:
+		local("cd ./articurate/motherlode && node app.js > /dev/null 2>&1 &")
+		print green("start_motherlode: Started Motherlode with Debug mode set to "+debug, bold=True)
+	else:
+		local("cd ./articurate/motherlode && node app.js > ../../motherlode.log 2>&1 &")
+		print green("start_motherlode: Started Motherlode with Debug mode set to "+debug, bold=True)
+
+
+#Stop Motherlode
+def stop_motherlode():
+	found = 'False';
+	for proc in psutil.process_iter():
+		try:
+			match = re.match(r'.*node.*',proc.name)
+			if match:
+				print green("stop_motherlode: Found process, stopping "+match.group()+".", bold=True)
+				proc.kill()
+				found = 'True'
+		except:
+			continue
+
+	#The below seems to not work, fix me.
+	if not found:
+		print green("stop_motherlode: Could not find process for Motherlode", bold=True)
